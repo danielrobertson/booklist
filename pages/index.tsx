852a834ab00674bf1ac1cf90a12420d96eda681b
Daniel Robertson <danielrobertson733@gmail.com>
2022-02-19 20:38:04 -0600

    book img header, cleanup starter files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import BookTile from '../components/BookTile'
 
 export default function Home() {
@@ -10,24 +11,17 @@ export default function Home() {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-start px-20 pt-20 text-center">
-        <h1 className="text-6xl font-bold">Booklist</h1>
+        <h1 className="flex items-center text-6xl font-bold">
+          <Image src="/book.png" alt="Book" width={70} height={70} />
+          <span className="pb-3">Booklist</span>
+        </h1>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around border sm:w-full">
           <BookTile />
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
+      <footer className="flex h-24 w-full items-center justify-center border-t"></footer>
     </div>
   )
 }
