ff52d81c639801349c0376c01b2510a23c1a079f
Daniel Robertson <danielrobertson733@gmail.com>
2023-11-24 18:16:34 -0600

    refactor: Next.js app router migration

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,19 @@ import BookTile from '../components/BookTile'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Booklist</title>
         <link rel="icon" href="/book.ico" />
       </Head>
+        <header className='flex px-5 py-2'>
+            <Image className='rounded-lg mr-3' src="/bookface.png" alt="Book" width={40} height={40} />
+          <h1 className="text-4xl font-bold">
+            booklistasdf
+          </h1>
+        </header>
 
       <main className="flex w-full flex-1 flex-col items-center justify-start px-20 pt-20 text-center">
-        <h1 className="flex items-center text-6xl font-bold">
-          <Image src="/book.png" alt="Book" width={70} height={70} />
-          <span className="pb-3">Booklist</span>
-        </h1>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around border sm:w-full">
           <BookTile />
