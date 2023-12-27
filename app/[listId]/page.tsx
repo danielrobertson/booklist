9041bddc9685ec25b1b17dad709bc3caf8206c0a
Daniel Robertson <danielrobertson733@gmail.com>
2023-12-27 13:55:08 -0600

    feat: list view page book tiles

--- app/[listId]/page.tsx
+++ app/[listId]/page.tsx
@@ -5,6 +5,8 @@ import prisma from '../../prisma/db';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
+const GOOGLE_VOLUME_API = 'https://www.googleapis.com/books/v1/volumes';
+
 export default async function ListPage({
   params,
 }: {
@@ -30,6 +32,15 @@ export default async function ListPage({
     list?.book_list_xref,
   );
 
+  const googleBooks = await Promise.all(
+    list?.book_list_xref.map(async (xref) => {
+      const bookId = xref.books?.google_book_id;
+      const response = await fetch(`${GOOGLE_VOLUME_API}/${bookId}`);
+      const data = await response.json();
+      return data;
+    }),
+  );
+
   return (
     <Theme>
       <Head>
@@ -48,15 +59,31 @@ export default async function ListPage({
       </header>
 
       <main className="mt-8 mx-auto max-w-lg px-4 sm:px-6 lg:px-8">
-        <ul>
-          {list?.book_list_xref.map((item) => (
-            <li key={item.id}>
-              {/* <img
-                  className="ml-2 inline-block h-12 rounded"
-                  src={item.book.volumeInfo.imageLinks?.thumbnail}
-                  alt=""
-                /> */}
-              <span className="ml-2">{item.books?.google_book_id}</span>
+        <div className="text-xs text-zinc-500 font-semibold">
+          {googleBooks.length} books
+        </div>
+        <ul className="mt-3">
+          {Array.from(googleBooks).map((googleBook: any) => (
+            <li key={googleBook.id}>
+              <a
+                href={googleBook.volumeInfo.infoLink}
+                className="flex items-start gap-2 mb-3 bg-gray-100 rounded shadow"
+                target="_blank"
+              >
+                <Image
+                  className="w-auto inline-block h-32 rounded"
+                  src={googleBook.volumeInfo.imageLinks.smallThumbnail}
+                  alt={googleBook.volumeInfo.title}
+                  width={85}
+                  height={128}
+                />
+                <div className="ml-2 mt-8 font-medium">
+                  {googleBook.volumeInfo.title}
+                  <div className="text-xs text-zinc-500">
+                    {googleBook.volumeInfo.authors[0] || ''}
+                  </div>
+                </div>
+              </a>
             </li>
           ))}
         </ul>
