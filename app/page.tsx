5d2f42b5e81e72425ea38f1165a676b3edbf42f2
Daniel Robertson <danielrobertson733@gmail.com>
2024-06-29 15:05:21 -0500

    feat: landing page with call-to-action

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import { Theme } from '@radix-ui/themes';
-
 import Header from '../components/Header';
+import Link from 'next/link';
 
 export default async function Home() {
   return (
@@ -12,7 +13,37 @@ export default async function Home() {
       </Head>
       <Header />
 
-      <main className="mt-8 mx-auto max-w-lg px-4 sm:px-6 lg:px-8"></main>
+      <main className="mt-8">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl ">
+              A better way to organize and share your book collections
+            </h1>
+            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl ">
+              From your shelf to the world—organize and share your books
+              effortlessly, making every read a shared adventure.
+            </p>
+
+            <Link
+              href="/lists/create"
+              className="px-6 py-3.5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg me-2 mb-2  focus:outline-none"
+            >
+              Create a List
+            </Link>
+          </div>
+          <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <Image
+              className=""
+              src="/book_open.png"
+              alt="Book"
+              width={400}
+              height={400}
+              loading="eager"
+              priority={true}
+            />
+          </div>
+        </div>
+      </main>
     </Theme>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import Image from 'next/image';
 import UserSignInOutButton from './UserSignInOutButton';
+import Link from 'next/link';
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className="mx-auto max-w-3xl flex justify-between p-3">
-      <div className="flex items-center px-2 py-2">
+    <header className="mx-auto max-w-screen-xl flex justify-between items-center px-3">
+      <Link href="/" className="flex items-center px-2 py-2">
         <Image
           className="rounded-md"
           src="/logo.png"
           alt="Book"
-          width={40}
-          height={40}
+          width={30}
+          height={30}
           loading="eager"
           priority={true}
         />
-        <h1 className="ml-1 text-2xl font-bold">Booklists</h1>
-      </div>
+        <h1 className="ml-1 text-xl font-bold">booklists</h1>
+      </Link>
       <UserSignInOutButton />
     </header>
   );
