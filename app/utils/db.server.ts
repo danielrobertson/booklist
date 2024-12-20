006afb8a0b4e531346b065c8c921acea77d6dcbf
Daniel Robertson <danielrobertson733@gmail.com>
2024-12-20 16:11:59 -0600

    chore: fix Cloudflare Pages Mongo env var

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -13,6 +13,10 @@ export interface ListModel {
 }
 
 const uri = process.env.MONGODB_URI!;
+if (!uri) {
+  throw new Error("MONGODB_URI environment variable is not set");
+}
+
 let mongodb: MongoClient;
 
 declare global {
